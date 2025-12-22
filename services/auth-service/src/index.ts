8d70e30f7a457a7c4b90ef7880f15568d9f22232
Rodrigo Moguillansky <romoguill@gmail.com>
2025-12-22 06:32:38 -0300

    Added shutdown util + posix listeners

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -2,20 +2,39 @@ import { createApp } from "./app";
 import { env } from "@/config/env";
 import { logger } from "@/utils/logger";
 import { SERVICE_NAME } from "./utils/constants";
+import { Server } from "node:https";
+import { IncomingMessage, ServerResponse } from "node:http";
 
 const main = async () => {
   const app = createApp();
+  let server: Server;
+
+  // ---- Start server ----
   try {
-    app.listen(env.PORT, () =>
+    server = app.listen(env.PORT, () =>
       logger.info(
         { port: env.PORT },
-        `${SERVICE_NAME} is running on port ${env.PORT}`
-      )
-    );
+        `${SERVICE_NAME} is running on port ${env.PORT}`,
+      ),
+    ) as Server<typeof IncomingMessage, typeof ServerResponse>;
   } catch (error) {
     logger.error(error, `Error starting ${SERVICE_NAME}`);
     process.exit(1);
   }
+
+  // ---- Shutdown server ----
+  const shutdown = () => {
+    logger.info({ serviceName: SERVICE_NAME }, "Shutting down");
+
+    // TODO: Add shutdown logic
+    Promise.all([])
+      .catch((error) => logger.error({ error }, "Error shutting down"))
+      .finally(() => server.close());
+  };
+
+  // ---- Shutdown server on signal ----
+  process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
 };
 
 void main();
