57f339d9022965d7f52cfbe2e845dc7856c39735
Rodrigo Moguillansky <romoguill@gmail.com>
2025-12-29 10:18:10 -0300

    Added common types used with rabbitmq

---
+++ packages/common/src/events/event-types.ts
@@ -0,0 +1,30 @@
+export type EventPayload = Record<string, unknown>;
+
+export interface DomainEvent<
+  TType extends string,
+  TPayload extends EventPayload,
+> {
+  type: TType;
+  payload: TPayload;
+  occurredAt: string;
+}
+
+export interface EventMetadata {
+  correlationId?: string;
+  causationId?: string;
+  version?: number;
+}
+
+export interface OutboundEvent<
+  TType extends string,
+  TPayload extends EventPayload,
+> extends DomainEvent<TType, TPayload> {
+  metadata?: EventMetadata;
+}
+
+export interface InboundEvent<
+  TType extends string,
+  TPayload extends EventPayload,
+> extends DomainEvent<TType, TPayload> {
+  metadata: EventMetadata;
+}
